2d8b515513a61b68059057cf30bef4b5bbc792d9
abbypan <abbypan@gmail.com>
2014-06-03 04:24:38 +0800

    request urls iter

--- lib/Novel/Robot/Browser.pm
+++ lib/Novel/Robot/Browser.pm
@@ -14,6 +14,7 @@ use Parallel::ForkManager;
 use Term::ProgressBar;
 use IO::Uncompress::Gunzip qw(gunzip);
 
+our $DEFAULT_URL_CONTENT = '';
 our %DEFAULT_HEADER = (
     'Accept' =>
       'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -45,52 +46,86 @@ sub _init_browser {
 } ## end sub init_browser
 
 sub request_urls {
-    my ( $self, $arr, %opt ) = @_;
+    my ( $self, $src_arr, %opt ) = @_;
+    # arr : url / { url => .. }
+    # data_sub => sub { my ($html_ref) = @_ ; ... }
+    
+    my $arr = $opt{select_url_sub} ? $opt{select_url_sub}->( $src_arr ) : $src_arr;
+    
+    my $iter_sub = sub {
+        my ($r) = @_;
+        return $opt{data_sub}->(@_) if($opt{no_auto_request_url});
+
+        my ( $url, $post_data ) =
+        ref($r) eq 'HASH' ? @{$r}{qw/url post_data/} 
+        : ( $r, undef );
+
+        my @procss_data = ($url, $post_data); 
+        if($url=~/^https?:/){
+            my $h = $self->request_url( $url, $post_data );
+            @procss_data = ( \$h );
+        }
+        my @return_data = exists $opt{data_sub} ?
+        $opt{data_sub}->(@procss_data) : @procss_data;
+        return @return_data;
+    };
 
     my $progress;
     $progress = Term::ProgressBar->new( { count => scalar(@$arr) } )
       if ( $opt{show_progress_bar} );
-    my $cnt = 0;
 
-    my @res;
+    my $cnt = 0;
+    my @res_arr;
+
     my $pm = Parallel::ForkManager->new( $self->{max_process_num} );
-    $pm->run_on_finish(
-        sub {
+    $pm->run_on_finish(sub {
             my ( $pid, $exit_code, $ident, $exit, $core, $data ) = @_;
+
             $cnt++;
             $progress->update($cnt) if ( $opt{show_progress_bar} );
             return unless ($data);
-            my ( $i, $r ) = @$data;
-            $res[$i] = $r;
-        }
-    );
 
-    $opt{request_sub} ||= sub {
-        my ($x) = @_;
-        my ( $url, $post_var ) =
-            exists $opt{url_sub} ? $opt{url_sub}->($x)
-          : ref($x) eq 'HASH' ? ( $x->{url}, $x->{post_var} )
-          : ref($x) eq 'ARRAY' ? @$x
-          :                      ( $x, undef );
-        return $self->request_url( $url, $post_var );
-    };
+            my ( $id, @res ) = @$data;
+            $res_arr[$id] = $#res==0 ? $res[0] : \@res;
+        });
 
     for my $i ( 0 .. $#$arr ) {
         my $pid = $pm->start and next;
-        my $x = $arr->[$i];
-
-        my $s = $opt{request_sub}->($x);
-        $s = $opt{deal_sub}->( $x, $s ) if ( exists $opt{deal_sub} );
-
-        $pm->finish( 0, [ $i, $s ] );
+        my @s = $iter_sub->($arr->[$i]);
+        $pm->finish( 0, [ $i, @s ] );
     }
+
     $pm->wait_all_children;
-    return \@res;
+
+    return \@res_arr;
 }
+
+sub request_urls_iter {
+    my ( $self, $url, %o ) = @_;
+
+    my $html = $self->request_url($url, $o{post_data});
+
+    my $info      = $o{info_sub}->( \$html )      || {};
+    my $data_list = $o{data_list_sub}->( \$html ) || [];
+
+    return ( $info, $data_list ) if ( $o{stop_sub}->( $info, $data_list ) );
+
+    my $url_list = $o{url_list_sub}->( \$html ) || [];
+    my $select_url_list = $o{select_url_sub}->( $url_list );
+    for my $u (@$select_url_list) {
+        return ( $info, $data_list ) if ( $o{stop_sub}->( $info, $data_list ) );
+
+        my $c = $self->request_url($u);
+        my $fs = $o{data_list_sub}->( \$c ) || [];
+        push @$data_list, @$fs;
+    }
+
+    return ( $info, $data_list );
+} ## end sub get_tiezi_ref
 
 sub request_url {
     my ( $self, $url, $form ) = @_;
-    return unless($url);
+    return $DEFAULT_URL_CONTENT unless($url);
 
     my $c;
     for my $i ( 1 .. $self->{retry} ) {
@@ -99,7 +134,7 @@ sub request_url {
         sleep 2;
     }
 
-    return $c;
+    return $c || $DEFAULT_URL_CONTENT;
 }
 
 sub request_url_simple {
@@ -109,7 +144,7 @@ sub request_url_simple {
         $form
       ? $self->{browser}->post_form( $url, $form )
       : $self->{browser}->get($url);
-    return unless ( $res->{success} );
+    return $DEFAULT_URL_CONTENT unless ( $res->{success} );
 
     my $html;
     my $content = $res->{content};
@@ -121,7 +156,7 @@ sub request_url_simple {
     my $charset = detect( $html || $content );
     my $r = decode( $charset, $html || $content, Encode::FB_XMLCREF );
 
-    return \$r;
+    return $r || $DEFAULT_URL_CONTENT;
 }
 
 1;
